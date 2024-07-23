5ed0fbd86870d0e4912878c4a1987df13c64b1ee
Sergey <sergo.ranes1@mail.ru>
2024-07-23 22:06:52 +0300

    ++

--- разряженные_матрицы/Matrix.py
+++ разряженные_матрицы/Matrix.py
@@ -14,3 +14,23 @@
     sumMatrix(matrix1: Matrix, matrix2: Matrix) => Result: Matrix
 
 """
+
+
+class Matrix:
+    def __init__(self, nameFile: str):
+        self.matrix = self.readMatrix(nameFile)
+
+    def readMatrix(self, namefile : str):
+        new = []
+        b = []
+        try:
+            file = open(namefile)
+            array = [row.strip() for row in file]
+            for i in range(len(array)):
+                buff = array[i].strip("")
+                for i in buff:
+                    b.append(int(i))
+                new.append(b)
+                b = []
+            return True, new
+        except FileNotFoundError:
